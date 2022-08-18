0d6ab6a60979c8c957f9ff9446b280d74445ec1a
Bell <jacobbrister71@gmail.com>
2022-08-17 22:12:15 -0400

    modified code to properly indent html

--- src/html-template.js
+++ src/html-template.js
@@ -2,17 +2,17 @@ function generateManager(employee) {
     const { name, email, id, office } = employee;
     
     return `
-    <div class="card">
-        <div class="card-header">
-            <h2 class="header-info">${name}</h2>
-            <h2 class="role header-info">Manager</h2>
-        </div>
-        <div class="card-body">
-            <p class="card-text">Id: ${id}</p>
-            <p class="card-text">Email: ${email}</p>
-            <p class="card-text">Office Number: ${office}</p>
-        </div>
-    </div>     
+                <div class="card">
+                    <div class="card-header">
+                        <h2 class="header-info">${name}</h2>
+                        <h2 class="role header-info">Manager</h2>
+                    </div>
+                    <div class="card-body">
+                        <p class="card-text">Id: ${id}</p>
+                        <p class="card-text">Email: ${email}</p>
+                        <p class="card-text">Office Number: ${office}</p>
+                    </div>
+                </div>     
     `
 }
 
@@ -20,17 +20,17 @@ function generateEngineer(employee) {
     const { name, email, id, github } = employee;
     
     return `
-    <div class="card">
-        <div class="card-header">
-            <h2 class="header-info">${name}</h2>
-            <h2 class="role header-info">Engineer</h2>
-        </div>
-        <div class="card-body">
-            <p class="card-text">Id: ${id}</p>
-            <p class="card-text">Email: ${email}</p>
-            <p class="card-text">Github: ${github}</p>
-        </div>
-    </div>     
+                <div class="card">
+                    <div class="card-header">
+                        <h2 class="header-info">${name}</h2>
+                        <h2 class="role header-info">Engineer</h2>
+                    </div>
+                    <div class="card-body">
+                        <p class="card-text">Id: ${id}</p>
+                        <p class="card-text">Email: ${email}</p>
+                        <p class="card-text">Github: ${github}</p>
+                    </div>
+                </div>     
     `
 }
 
@@ -38,17 +38,17 @@ function generateIntern(employee) {
     const { name, email, id, school } = employee;
     
     return `
-    <div class="card">
-        <div class="card-header">
-            <h2 class="header-info">${name}</h2>
-            <h2 class="role header-info">Intern</h2>
-        </div>
-        <div class="card-body">
-            <p class="card-text">Id: ${id}</p>
-            <p class="card-text">Email: ${email}</p>
-            <p class="card-text">School: ${school}</p>
-        </div>
-    </div>     
+                <div class="card">
+                    <div class="card-header">
+                        <h2 class="header-info">${name}</h2>
+                        <h2 class="role header-info">Intern</h2>
+                    </div>
+                    <div class="card-body">
+                        <p class="card-text">Id: ${id}</p>
+                        <p class="card-text">Email: ${email}</p>
+                        <p class="card-text">School: ${school}</p>
+                    </div>
+                </div>     
     `
 }
 
